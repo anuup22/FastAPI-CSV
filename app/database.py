81ec9af8b96ce73104ad21aabd4507da7b5324bd
Anup Gupta <anuup22.cs@gmail.com>
2024-10-30 11:59:29 +0530

    Refactor database connection to use environment variables for configuration

--- app/database.py
+++ app/database.py
@@ -1,9 +1,22 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "postgresql://myroot:mypassword@localhost/csvdb"
+# Environment variables from .env file
+load_dotenv()
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# Database connection details from environment variables
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+
+# Database URL
+DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
